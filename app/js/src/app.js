ce195213b8a281acc1c81fc05ec1e2841c07e821
Manuel Gegenhuber <Manuel.gegenhuber.mg@gmail.com>
2018-12-30 19:27:59 +0100

    header finished

--- app/js/src/app.js
+++ app/js/src/app.js
@@ -3,13 +3,38 @@ import {onScroll,anchorElement_outofView} from 'scroll';
 
 domReady(() =>{
 
+  let navigation_desktop = document.getElementById("navigation__desktop ");
+
   onScroll(
     (event)=>{
-      //console.log(event);
+      //on scroll Up and Down
       anchorElement_outofView(
         document.getElementsByClassName('header__logo-box ')[0],
         document.getElementsByClassName('navigation__logo-box')[0]
       );
+
+      if(window.scrollY  < 100){
+        navigation_desktop.style.backgroundColor = "transparent";
+      }
+    },
+    (event) =>{
+      //on scrolling down
+      if(navigation_desktop.classList.contains("uk-position-fixed")){
+        navigation_desktop.classList.add("uk-position-absolute");
+        navigation_desktop.classList.remove("uk-position-fixed");
+      }
+    },
+    (event) =>{
+      //on scrolling up
+      if(navigation_desktop.classList.contains("uk-position-absolute")){
+        navigation_desktop.classList.add("uk-position-fixed");
+        
+        if(window.scrollY  > 101){
+          navigation_desktop.style.backgroundColor = "#222";
+        }
+
+        navigation_desktop.classList.remove("uk-position-absolute");
+      }
     }
 
   );
--- app/js/src/scroll.js
+++ app/js/src/scroll.js
@@ -1,11 +1,18 @@
-export function onScroll(cb){
+export function onScroll(cbBoth, cbScrollDown, cbScrollUp){
     window.addEventListener('scroll', function(event) {
-        cb(event);
+        cbBoth(event);
+        if(this.oldScroll > this.scrollY){
+            cbScrollUp(event);
+        }else{
+            cbScrollDown(event);
+        }
+        //both
+        this.oldScroll = this.scrollY;
     });
 };
 
 export function anchorElement_outofView(anchorElement, modifyElement) {
-    if(window.pageYOffset > anchorElement.offsetTop + anchorElement.offsetHeight){
+    if(window.pageYOffset > anchorElement.offsetTop - (anchorElement.offsetHeight / 2)){
         modifyElement.style.display = 'block';
     }else{
         modifyElement.style.display = 'none';
